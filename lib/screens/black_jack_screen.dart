c98a21865c654a273ee4630a4a82e2f1bfccf9c4
Konstantin <kotplansh@gmail.com>
2022-07-10 15:31:08 +0300

    first init

---
+++ lib/screens/black_jack_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart ';
+
+class BlackJackScreen extends StatefulWidget {
+
+
+  @override
+  State<BlackJackScreen> createState() => _BlackJackScreenState();
+}
+
+class _BlackJackScreenState extends State<BlackJackScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
